697dc911bca2f81d1bb92263c8d532d2f8ec2b16
tmar89 <tmar89@7f3166ef-3b52-0410-ad74-e9bab01e5e52>
2008-11-06 16:27:00 +0000

    Let CSVDecimator run as application

--- trunk/rtmd/RTMD_JAR/src/edu/lehigh/nees/util/CSVDecimator.java
+++ trunk/rtmd/RTMD_JAR/src/edu/lehigh/nees/util/CSVDecimator.java
@@ -16,6 +16,7 @@ import javax.swing.*;
  * <b><u>Revisions</u></b><br>
  * 22 Mar 06  T. Marullo  Initial
  *  6 Apr 07  T. Marullo  Converted to Applet
+ *  6 Nov 08  T. Marullo  Allowed to run as Application
  * 
  ********************************/
 public class CSVDecimator extends JApplet implements ActionListener {
@@ -212,6 +213,38 @@ public class CSVDecimator extends JApplet implements ActionListener {
             
         JOptionPane.showMessageDialog(null,"Decimation Complete");
     }            
+    
+    public static void main(String args[]) {
+    	//Create the frame this applet will run in
+    	JFrame appletFrame = new JFrame("CSV Decimator");
+
+    	//The frame needs a layout manager, use the GridLayout to maximize
+    	//the applet size to the frame.
+    	appletFrame.setLayout(new GridLayout(1,0));
+
+    	//Have to give the frame a size before it is visible
+        appletFrame.resize(250,280);
+
+		//Make the frame appear on the screen. You should make the frame appear
+		//before you call the applet's init method. On some Java implementations,
+		//some of the graphics information is not available until there is a frame.
+		//If your applet uses certain graphics functions like getGraphics() in the
+		//init method, it may fail unless there is a frame already created and
+		//showing.
+        appletFrame.show();
+
+        //Create an instance of the applet
+        JApplet myApplet = new CSVDecimator();
+
+        //Add the applet to the frame
+        appletFrame.add(myApplet);
+
+        //Initialize and start the applet
+        myApplet.init();
+        myApplet.start();
+
+    }
+
     
     // Variables    
     private int numLines;     
